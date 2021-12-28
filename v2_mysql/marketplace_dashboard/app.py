59f90208a5a849a4cb113956768680f3f8753eeb
theodenking <mdettmar@PickleRick.home>
2021-12-28 13:33:47 -0500

    updated daily volume query

---
+++ v2_mysql/marketplace_dashboard/app.py
@@ -0,0 +1,115 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+import os
+import json
+import pandas as pd
+from sqlalchemy import create_engine
+
+app = dash.Dash(__name__)
+
+# read in data
+sql_credential = os.path.join("constants", "mysql_credential.json")
+with open(sql_credential) as f:
+    mysql_credentials = json.loads(f.read())
+engine = create_engine(
+    "mysql+pymysql://{user}:{pw}@{host}/{db}".format(
+    user=mysql_credentials['username'], 
+    pw=mysql_credentials['pw'], 
+    host=mysql_credentials['host'], 
+    db="treasure"
+    )
+)
+connection = engine.connect()
+
+marketplace_sales_list = []
+marketplace_sales_query = connection.execute('SELECT * FROM treasure.marketplace_sales')
+for row in marketplace_sales_query:
+    marketplace_sales_list.append(row)
+marketplace_sales = pd.DataFrame(marketplace_sales_list)
+marketplace_sales.columns=list(marketplace_sales_query.keys())
+connection.close()
+engine.dispose()
+
+
+df = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+available_indicators = df['Indicator Name'].unique()
+
+app.layout = html.Div([
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Fertility rate, total (births per woman)'
+            ),
+            dcc.RadioItems(
+                id='xaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Life expectancy at birth, total (years)'
+            ),
+            dcc.RadioItems(
+                id='yaxis-type',
+                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
+                value='Linear',
+                labelStyle={'display': 'inline-block'}
+            )
+        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        id='year--slider',
+        min=df['Year'].min(),
+        max=df['Year'].max(),
+        value=df['Year'].max(),
+        marks={str(year): str(year) for year in df['Year'].unique()},
+        step=None
+    )
+])
+
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis-column', 'value'),
+    Input('yaxis-column', 'value'),
+    Input('xaxis-type', 'value'),
+    Input('yaxis-type', 'value'),
+    Input('year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df[df['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    fig.update_xaxes(title=xaxis_column_name,
+                     type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name,
+                     type='linear' if yaxis_type == 'Linear' else 'log')
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
